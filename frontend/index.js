7a2a8843e80bf18b7f571bb203a6c5e025f928ed
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-02-13 16:18:09 +0900

    card open-api
    open-api활용 카드에 요청 한 데이터 적용

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,43 @@
+$(document).ready(function () {
+  $("#cards-box").empty();
+  listing();
+});
+
+function listing() {
+  $.ajax({
+    type: "GET",
+    url: "http://spartacodingclub.shop/post",
+    data: {},
+    success: function (response) {
+      let rows = response["articles"];
+      for (let i = 0; i < rows.length; i++) {
+        let comment = rows[i]["comment"];
+        let desc = rows[i]["desc"];
+        let image = rows[i]["image"];
+        let title = rows[i]["title"];
+        let url = rows[i]["url"];
+
+        let temp_html = `<div class="card">
+        <img
+          class="card-img-top"
+          src=${image}
+          alt="Card image cap"
+        />
+        <div class="card-body">
+          <a href=${url} class="card-title"
+            >${title}</a
+          >
+          <p class="card-text">
+            ${desc}
+          </p>
+          <p class="card-coment">${comment}</p>
+        </div>`;
+        $("#cards-box").append(temp_html);
+      }
+    },
+  });
+}
+
 function openclose() {
   let status = $("#posting-box").css("display");
   if (status == "block") {
